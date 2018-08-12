bd3c65edeac4fba9d208c8e447bce7f38a17e6c2
Emily Asch <e.asch@me.com>
2018-08-12 18:31:21 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -2,8 +2,8 @@ def square_array(array)
   # your code here
   returnarr = []
   
-  array.each do |num|
-    returnarr << num*num
+  array.each do |number|
+    returnarr << number*number
   end
   
   return returnarr
